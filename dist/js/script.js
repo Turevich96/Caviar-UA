e17e52acb9ea180e18b2dcbe289f6d63e56e25ff
Yura Turevich <oleo.ukraine@gmail.com>
2020-12-03 23:46:18 +0200

    add js

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,29 +12,44 @@ $("#slider").slick({
 
 // Burger Menu
 
-function burgerMenu(selector) {
-  let menu = $(selector);
-  let button = menu.find(".burger-menu__button", ".burger-menu__button__lines");
-  let links = menu.find(".burger-menu__nav__link");
-  let overlay = menu.find(".burger-menu__overlay");
+// function burgerMenu(selector) {
+//   let menu = $(selector);
+//   let button = menu.find(".burger-menu__button", ".burger-menu__button__lines");
+//   let links = menu.find(".burger-menu__nav__link");
+//   let overlay = menu.find(".burger-menu__overlay");
 
-  button.on("click", (e) => {
-    e.preventDefault();
-    toggleMenu();
-  });
+//   button.on("click", (e) => {
+//     e.preventDefault();
+//     toggleMenu();
+//   });
 
-  links.on("click", () => toggleMenu());
-  overlay.on("click", () => toggleMenu());
+//   links.on("click", () => toggleMenu());
+//   overlay.on("click", () => toggleMenu());
 
-  function toggleMenu() {
-    menu.toggleClass("burger-menu__active");
+//   function toggleMenu() {
+//     menu.toggleClass("burger-menu__active");
 
-    if (menu.hasClass("burger-menu__active")) {
-      $("body").css("overlow", "hidden");
-    } else {
-      $("body").css("overlow", "visible");
-    }
-  }
+//     if (menu.hasClass("burger-menu__active")) {
+//       $("body").css("overlow", "hidden");
+//     } else {
+//       $("body").css("overlow", "visible");
+//     }
+//   }
+// }
+
+// burgerMenu(".burger-menu");
+
+// btn
+let btn1 = document.querySelector(".home__description__btn");
+let popap1 = document.querySelector(".modal-dialog");
+let btnClose = document.querySelector(".close");
+// console.log(popap1);
+function displayBlock() {
+  popap1.classList.add("block");
+}
+function displayNone() {
+  popap1.classList.remove("block");
 }
 
-burgerMenu(".burger-menu");
+btn1.addEventListener("mouseup", displayBlock);
+btnClose.addEventListener("mouseup", displayNone);
